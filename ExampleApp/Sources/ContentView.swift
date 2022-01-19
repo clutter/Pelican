a45f80b1008c52bb528c01fa4c061a666fd6dbd2
Robert Manson <robmanson@gmail.com>
2022-01-19 15:20:56 -0800

    Move Tests and Example App
    Creates a stub app to host the Pelican Tests and replaces existing
    example app.

---
+++ ExampleApp/Sources/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  PodApp
+//
+//  Created by Robert Manson on 1/19/22.
+//
+
+import Pelican
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Pelican Unit Test Driver")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
